9b33f9a2fed904d787606937f140f6bb633e5f7f
pragath17MA <pragathannadurai03122005@gmail.com>
2026-03-12 19:19:18 +0530

    problem

---
+++ Read from STDIN, Write to STDOUT.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();   
+        sc.nextLine();          
+        String S = sc.nextLine(); 
+        System.out.println(N * 2); 
+        System.out.println(S);    
+    }
+}
